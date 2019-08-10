788419d77d2bd02b451a70744de74f990d2f37ab
Faizan-Ahmed-Khan <mailtofaizan28@gmail.com>
2019-08-10 19:58:01 +0530

    Initial Stub of the Project

---
+++ edubrains/src/main/java/com/learning/edubrains/EdubrainsApplication.java
@@ -0,0 +1,13 @@
+package com.learning.edubrains;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdubrainsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdubrainsApplication.class, args);
+	}
+
+}
---
+++ edubrains/src/test/java/com/learning/edubrains/EdubrainsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.learning.edubrains;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EdubrainsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
